9da4f9036337dcd20fb335697f762fccefd402aa
ASUS <hokure.04@gmail.com>
2025-04-09 22:10:43 +0300

    transaction creation and get functions added

--- deposit/main.go
+++ deposit/main.go
@@ -1,17 +1,46 @@
-package deposit
+package main
 
 import (
+	"deposit/operations"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(writer, "Pong!")
-	})
-	fmt.Println("Listening on port 8081")
-	err := http.ListenAndServe(":8081", nil)
+func getTransactions(writer http.ResponseWriter, request *http.Request) {
+	transactions := operations.GetAllTransactions()
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(transactions)
+}
+
+func transferMoney(writer http.ResponseWriter, request *http.Request) {
+	var tx operations.Transaction
+	err := json.NewDecoder(request.Body).Decode(&tx)
 	if err != nil {
-		panic(err)
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	newTx, err := operations.Transfer(tx.FromAccount, tx.ToAccount, tx.Amount)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(newTx)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/transactions", getTransactions).Methods("GET")
+	router.HandleFunc("/transfer", transferMoney).Methods("POST")
+
+	fmt.Println("Listening on port 8081")
+	err := http.ListenAndServe(":8081", router)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
---
+++ deposit/operations/transaction.go
@@ -0,0 +1,37 @@
+package operations
+
+import "fmt"
+
+type Transaction struct {
+	ID          string  `json:"id"`
+	Amount      float64 `json:"amount"`
+	FromAccount string  `json:"from_account"`
+	ToAccount   string  `json:"to_account"`
+	Type        string  `json:"type"`
+}
+
+var Transactions []Transaction
+
+func CreateTransaction(id, fromAccount, toAccount string, amount float64, transactionType string) Transaction {
+	tx := Transaction{
+		ID:          id,
+		FromAccount: fromAccount,
+		ToAccount:   toAccount,
+		Amount:      amount,
+		Type:        transactionType,
+	}
+	Transactions = append(Transactions, tx)
+	return tx
+}
+
+func GetAllTransactions() []Transaction {
+	return Transactions
+}
+
+func Transfer(fromAccount, toAccount string, amount float64) (Transaction, error) {
+	if amount <= 0 {
+		return Transaction{}, fmt.Errorf("Amount can't be zero or lower")
+	}
+	tx := CreateTransaction(fmt.Sprintf("%d", len(Transactions)+1), fromAccount, toAccount, amount, "transfer")
+	return tx, nil
+}
