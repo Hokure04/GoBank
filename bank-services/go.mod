ad255f975d90e508645e8c49935d4fedbc1fde4d
jaba <368601@edu.itmo.ru>
2025-05-01 01:37:03 +0300

    add test mail sender

--- bank-services/go.mod
+++ bank-services/go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 require (
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
@@ -12,4 +13,5 @@ require (
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250425173222-7b384671a197 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
---
+++ bank-services/mail/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/gomail.v2"
+)
+
+func main() {
+	// Create a new message
+	message := gomail.NewMessage()
+
+	// Set email headers
+	message.SetHeader("From", "go.bank.03@mail.ru")
+	message.SetHeader("To", "random@gmail.com")
+	message.SetHeader("Subject", "Hello from the Mailtrap team")
+
+	// Set email body
+	message.SetBody("text/plain", "This is the Test Body")
+
+	// Set up the SMTP dialer Vjrm6i0L46zzKntnEEkD
+	dialer := gomail.NewDialer("smtp.mail.ru", 465, "go.bank.03@mail.ru", "password")
+
+	//// Send the email
+	if err := dialer.DialAndSend(message); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Email sent successfully!")
+	}
+}
